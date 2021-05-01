57dbc3c6c1f3debfa760dbd39e2b86dea1775899
allansham <allan.sham@trapezegroup.com>
2021-05-01 18:35:21 +0100

    Add taiga ui

--- apps/koffee-beans/src/app/app.module.ts
+++ apps/koffee-beans/src/app/app.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { TuiRootModule, TuiButtonModule } from '@taiga-ui/core';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, HttpClientModule],
+  imports: [BrowserModule, HttpClientModule, TuiRootModule, TuiButtonModule],
   providers: [],
   bootstrap: [AppComponent],
 })
