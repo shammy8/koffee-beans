509d60fcc91582e8b0d40dc817b4827c24e46e50
Allan Sham <allansham@hotmail.com>
2021-05-08 17:00:45 +0100

    Install mongo stuff and connect to it

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -1,12 +1,20 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { SpendingsModule } from './spendings/spendings.module';
 import { PlaidModule } from './plaid/plaid.module';
 
 @Module({
-  imports: [ConfigModule.forRoot(), SpendingsModule, PlaidModule],
+  imports: [
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(
+      `mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@cluster0.0rnyh.mongodb.net/koffeeBeans?retryWrites=true&w=majority`
+    ),
+    SpendingsModule,
+    PlaidModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
